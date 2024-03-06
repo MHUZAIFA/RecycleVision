5e6b88295564919494a75cae661e539c7a2571f6
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2024-03-06 16:17:30 -0500

    configured tabs - js

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,28 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
+import React from "react";
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue", headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Dashboard",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="camera"
+        options={{
+          title: "Camera",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="camera" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Tab() {
+  return (
+    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+      <Text>Dashboard</Text>
+    </View>
+  );
+}
