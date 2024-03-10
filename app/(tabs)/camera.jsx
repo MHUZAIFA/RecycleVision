0ad4ab7448828ab01cc3fb369cf6fc7f3cec9411
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2024-03-09 22:45:04 -0500

    implement camera

--- app/(tabs)/camera.jsx
+++ app/(tabs)/camera.jsx
@@ -1,62 +1,14 @@
 import { Camera, CameraType } from "expo-camera";
+import { SaveFormat, manipulateAsync } from "expo-image-manipulator";
 import { useRef, useState } from "react";
 import { Button, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function CameraScreen() {
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [detectedLabel, setDetectedLabel] = useState(null);
+  const [confidence, setConfidence] = useState(null);
   const cameraRef = useRef(null);
-
-  const takePicture = async () => {
-    if (cameraRef.current) {
-      const result = await cameraRef.current.takePictureAsync({
-        aspect: [4, 3],
-        allowsEditing: true,
-        quality: 1,
-        base64: true,
-        fixOrientation: true,
-        exif: true,
-      });
-      const data = createFormData(result.uri);
-      sendImageToModel(data);
-    }
-  };
-
-  const createFormData = (uri) => {
-    const fileName = uri.split("/").pop();
-    const fileType = fileName.split(".").pop();
-    const formData = new FormData();
-    formData.append("file", {
-      uri,
-      name: fileName,
-      type: `image/${fileType}`,
-    });
-
-    return formData;
-  };
-
-  const sendImageToModel = async (image) => {
-    await fetch(
-      "https://api-inference.huggingface.co/models/plsakr/vit-garbage-classification-v2",
-      {
-        headers: {
-          Authorization: "Bearer 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",
-          Accept: "application/json",
-        },
-        method: "POST",
-        body: image,
-      }
-    )
-      .then((response) => {
-        console.log(response, "RESPONSE");
-      })
-      .then((result) => {
-        console.log(result, "RESULT");
-      })
-      .catch((error) => {
-        console.log(error, "ERROR");
-      });
-  };
 
   if (!permission) {
     return <View />;
@@ -78,18 +30,73 @@ export default function CameraScreen() {
     );
   }
 
+  const detectImage = async () => {
+    if (cameraRef.current) {
+      const pic = await cameraRef.current?.takePictureAsync({
+        base64: true,
+        fixOrientation: true,
+      });
+      const resizedPic = await manipulateAsync(
+        pic.uri,
+        [{ resize: { width: 224, height: 224 } }],
+        { compress: 0.4, format: SaveFormat.JPEG, base64: true }
+      );
+      const response = await fetch(resizedPic.uri);
+      const blob = await response.blob();
+      const arrayBuffer = await new Response(blob).arrayBuffer();
+      sendImageToModel(arrayBuffer);
+    }
+  };
+
+  const sendImageToModel = async (arrayBuffer) => {
+    try {
+      const result = await fetch(
+        "https://api-inference.huggingface.co/models/plsakr/vit-garbage-classification-v2",
+        {
+          headers: {
+            Authorization: "Bearer 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",
+          },
+          method: "POST",
+          body: arrayBuffer,
+        }
+      );
+      const resultJson = await result.json();
+      const { label, score } = resultJson[0];
+      label && setDetectedLabel(label);
+      score && setConfidence(Math.round(score.toFixed(2) * 100));
+    } catch (e) {
+      console.log("Failed", e);
+    }
+  };
+
+  setTimeout(() => {
+    setDetectedLabel(null);
+    setConfidence(null);
+  }, 5000);
+
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type} ref={cameraRef}>
+      <Camera
+        style={styles.camera}
+        type={type}
+        autoFocus={Camera.Constants.AutoFocus.on}
+        ref={cameraRef}>
         <View style={styles.buttonContainer}>
           <Pressable style={styles.button} onPress={toggleCameraType}>
             <Text style={styles.text}>Flip Camera</Text>
           </Pressable>
-          <Pressable style={styles.button} onPress={takePicture}>
+          <Pressable style={styles.button} onPress={detectImage}>
             <Text style={styles.text}>Take Picture</Text>
           </Pressable>
         </View>
       </Camera>
+      {detectedLabel && confidence && (
+        <View style={styles.resultContainer}>
+          <Text style={styles.text}>
+            {detectedLabel} - {confidence}%
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -118,5 +125,17 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     color: "white",
+  },
+  resultContainer: {
+    position: "absolute",
+    top: "50%",
+    left: 0,
+    right: 0,
+    height: 50,
+    marginTop: -25,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    padding: 16,
   },
 });
