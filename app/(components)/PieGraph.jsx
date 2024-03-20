844e3b124a0d29c342f93952fe85241a5bcf6cfa
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2024-03-20 01:02:37 -0400

    update coupon

--- app/(components)/PieGraph.jsx
+++ app/(components)/PieGraph.jsx
@@ -1,49 +1,50 @@
-import { PieChart } from "react-native-chart-kit";
 import { Text, View } from "react-native";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { PieChart } from "react-native-chart-kit";
 
 const PieGraph = (props) => {
-const colorMapping = {
-  'Paper & Cardboard': '#0000FF',
-  'Other Recyclables': '#008000',
-  'Food Organics': '#A52A2A',
-  'General Waste': '#000000'
-};
-
-const chartData = Object.keys(props.pieData).map(key => {
-  let newKey = key;
-
-  // Rename the keys
-  if (key === 'metal/glass/plastic') {
-    newKey = 'Other Recyclables';
-  }
-  if (key === 'cardboard/paper') {
-    newKey = 'Paper & Cardboard';
-  }
-  if (key === 'organic') {
-    newKey = 'Food Organics';
-  }
-  if (key === 'trash') {
-    newKey = 'General Waste';
-  }
-
-  return {
-    name: newKey,
-    population: props.pieData[key],
-    color: colorMapping[newKey],
-    legendFontColor: '#7F7F7F',
-    legendFontSize: 12,
+  const colorMapping = {
+    "Paper & Cardboard": "#0000FF",
+    "Other Recyclables": "#008000",
+    "Food Organics": "#A52A2A",
+    "General Waste": "#000000",
   };
-});
+
+  const chartData = Object.keys(props.pieData).map((key) => {
+    let newKey = key;
+
+    // Rename the keys
+    if (key === "metal/glass/plastic") {
+      newKey = "Other Recyclables";
+    }
+    if (key === "cardboard/paper") {
+      newKey = "Paper & Cardboard";
+    }
+    if (key === "organic") {
+      newKey = "Food Organics";
+    }
+    if (key === "trash") {
+      newKey = "General Waste";
+    }
+
+    return {
+      name: newKey,
+      population: props.pieData[key],
+      color: colorMapping[newKey],
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 12,
+    };
+  });
 
   if (!chartData.length) {
     return <Text>No data available</Text>;
   }
 
   return (
-    <View className="w-[355px] h-[260px] flex flex-col rounded-xl border-2 border-gray-400">
-      <View className="flex flex-row justify-center mt-5 mb-5">
-        <Text className="text-sm text-gray-500" style={{ fontSize: 20 }}>Types of Items Scanned</Text>
+    <View className="w-[355px] h-[240px] flex flex-col rounded-xl border-2 border-gray-400">
+      <View className="flex flex-row justify-center mt-5 mb-2">
+        <Text className="text-sm text-gray-500" style={{ fontSize: 20 }}>
+          Types of Items Scanned
+        </Text>
       </View>
       <PieChart
         data={chartData}
